3ee9ab993c083765b3a175fe074b6a30f1036c90
Letícia Roque Lima Oliveira <62360470+letty-Rock@users.noreply.github.com>
2022-02-02 15:57:15 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -1,39 +1,36 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo de R$ {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo de R$ {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self,valor):
-        self.saldo += valor
+        self.__saldo += valor
         print("Você depositou R$ {}".format(valor))
+        
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+    
     def sacar(self,valor):
-        self.saldo -= valor
+        if(self.__pode_sacar(valor)):
+        self.__saldo -= valor  
         print("Você sacou R$ {}".format(valor))
+        else:
+            print("O valor {} é maior que o seu limmite".format(valor))
+     
+        
+        
     def transferir(self,valor,destino):
-         self.sacar(valor)
+         self.__sacar(valor)
          destino.deposita(valor)
-<<<<<<< HEAD
 
-    @property
-    def saldo(self):
-        return self.__saldo
-
-    @property
-    def titular(self):
-        return self.__titular
-
-    @property
-    def limite(self):
-        return self.__limite
-
-=======
    
-       @property
+    @property
     def saldo(self):
         return self.__saldo
     
@@ -45,7 +42,7 @@ class Conta:
     def limite(self):
         return self.__limite
     
->>>>>>> 10779894925afc8fd17b452e032ae12426cd32e5
+
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
