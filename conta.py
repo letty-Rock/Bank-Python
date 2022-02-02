10779894925afc8fd17b452e032ae12426cd32e5
Letícia Roque Lima Oliveira <62360470+letty-Rock@users.noreply.github.com>
2022-02-02 15:18:38 -0300

    Add files via upload

--- conta.py
+++ conta.py
@@ -1,33 +1,36 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        self.__numero = numero
-        self.__titular = titular
-        self.__saldo = saldo
-        self.__limite = limite
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
 
     def extrato(self):
-        print("Saldo de R$ {} do titular {}".format(self.__saldo, self.__titular))
+        print("Saldo de R$ {} do titular {}".format(self.saldo, self.titular))
 
-    def deposita(self, valor):
-        self.__saldo += valor
+    def deposita(self,valor):
+        self.saldo += valor
         print("Você depositou R$ {}".format(valor))
-
-    def sacar(self, valor):
-        self.__saldo -= valor
+    def sacar(self,valor):
+        self.saldo -= valor
         print("Você sacou R$ {}".format(valor))
-
-    def transferir(self, valor, destino):
+    def transferir(self,valor,destino):
          self.sacar(valor)
          destino.deposita(valor)
-
-    def get_saldo(self):
+   
+       @property
+    def saldo(self):
         return self.__saldo
-
-    def get_titular(self):
+    
+    @property
+    def titular(self):
         return self.__titular
-
-    def get_limite(self):
+    
+    @property
+    def limite(self):
         return self.__limite
-
-    def set_limite(self, limite):
+    
+    @limite.setter
+    def limite(self, limite):
         self.__limite = limite
+    
