c72c948136bcf57af7343f32da09b886d7eef14b
Letícia Roque Lima Oliveira <62360470+letty-Rock@users.noreply.github.com>
2022-02-02 12:07:09 -0300

    .

--- conta.py
+++ conta.py
@@ -23,7 +23,7 @@ class Conta:
     def get_titular(self):
         return self.__titular
 
-    def get__limite(self):
+    def get_limite(self):
         return self.__limite
     def set_limite(self,limite):
         self.__limite = limite     
